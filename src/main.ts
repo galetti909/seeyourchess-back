e1d442ec73606b8f7cb202a0bcc593c262a827d0
galetti909 <joaogaletti@gmail.com>
2025-09-29 23:23:06 +0000

    build: swagger setup

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +15,14 @@ async function bootstrap() {
       },
     }),
   );
+
+  const docs_config = new DocumentBuilder()
+    .setTitle('API')
+    .setDescription('Documentação da API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, docs_config);
+  SwaggerModule.setup('docs', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 }
