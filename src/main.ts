2b4abd0925a5bf677b4eb26a5e1f2404bb4b69c5
galetti909 <joaogaletti@gmail.com>
2025-10-18 18:30:24 +0000

    feat: catch and log app bootstrap errors

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
@@ -28,4 +28,7 @@ async function bootstrap() {
 
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+bootstrap().catch((err) => {
+  Logger.error('❌ Error during app bootstrap:', err);
+  process.exit(1);
+});
